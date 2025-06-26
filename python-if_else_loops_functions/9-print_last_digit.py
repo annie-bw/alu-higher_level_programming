164cc862f1f2965e5e46d1009d7bcf20182e2560
BWIZA Annie <a.bwiza1@alustudent.com>
2025-06-26 11:45:51 -0700

    aadd

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        number *= -1
-    print(number % 10)
-    return number % 10
+    last = abs(number) % 10
+    print(last, end="")
+    return last
