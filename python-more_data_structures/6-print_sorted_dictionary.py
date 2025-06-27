2913affe7fbef044e9e198c29a51bd1148a16349
BWIZA Annie <a.bwiza1@alustudent.com>
2025-06-27 05:50:44 -0700

    l

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     keys = list(a_dictionary.keys())
-    
     # Simple bubble sort to sort the keys alphabetically (since we can't use sorted())
     for i in range(len(keys)):
         for j in range(i + 1, len(keys)):
             if keys[i] > keys[j]:
-                keys[i], keys[j] = keys[j], keys[i]
-    
+                keys[i], keys[j] = keys[j], keys[i] 
     for key in keys:
         print(f"{key}: {a_dictionary[key]}")
