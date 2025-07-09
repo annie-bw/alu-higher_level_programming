bd8c9473985a4fd5fa5c4e8325734b22fc6ff41a
annie-bw <a.bwiza1@alustudent.com>
2025-07-09 22:03:08 +0200

    6 base geo answer

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a class named BaseGeometry
+with a public method that raises an exception.
+"""
+
+class BaseGeometry:
+    """
+    A base geometry class with an unimplemented area method.
+    """
+
+    def area(self):
+        """
+        Raises an Exception indicating that area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
