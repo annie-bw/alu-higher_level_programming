4c80064090c5e07c208fe89b993d06359f8a74b6
annie-bw <a.bwiza1@alustudent.com>
2025-07-18 20:35:06 +0200

    changes to 7th file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,10 +10,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 
 if os.path.exists(filename):
-    items = load_from_json_file(filename)
+    my_list = load_from_json_file(filename)
 else:
-    items = []
+    my_list = []
 
-items.extend(sys.argv[1:])
+my_list.extend(sys.argv[1:])
 
-save_to_json_file(items, filename)
+save_to_json_file(my_list, filename)
